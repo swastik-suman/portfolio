0dbf4cae2d71927d693fcacb7678201ac2327e54
Swastik Suman <swastiksuman28@gmail.com>
2026-07-07 13:11:46 +0530

    Update page.tsx
    adding the cerfification to the page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Skills } from "@/components/Skills";
 import { Projects } from "@/components/Projects";
 import { Experience } from "@/components/Experience";
 import { Education } from "@/components/Education";
+import { Certifications } from "@/components/Certifications";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 
@@ -19,6 +20,7 @@ export default function Home() {
         <Projects />
         <Experience />
         <Education />
+        <Certifications />
         <Contact />
       </main>
       <Footer />
